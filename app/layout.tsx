dbce204029599fb63693c6729122cc928ceb38b9
srushteesingh <srushteesingh@gmail.com>
2026-03-03 01:20:45 +0530

    Improve global layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body className="bg-black text-white antialiased">
         {children}
       </body>
     </html>
