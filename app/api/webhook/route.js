3427587807b10f9240dbe335dd775ae05e39face
srushteesingh <srushteesingh@gmail.com>
2026-03-06 15:19:23 +0530

    fix slot command

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -33,7 +33,7 @@ export async function POST(req) {
     const from = message.from;
     let reply = "";
 
-    if (text === "1") {
+    if (text === "slots") {
       const { data } = await supabase
         .from("slots")
         .select("*")
